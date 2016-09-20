d7bb3e158ae8369b6438960f08ebaca886475b24
Darren King <darrenbrettking@hotmail.com>
2016-09-19 17:57:50 -0700

    Initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './book/book.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BooksComponent,
+    BookComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
